6932683b6ed81c7685631398ec6d04818bb5ac09
sujal <sujalkarki1904@gmail.com>
2026-01-02 17:09:54 +0545

    added local host for testing

--- src/Database/connection.js
+++ src/Database/connection.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const API = import.meta.env.VITE_API_URL
+const API = import.meta.env.VITE_API_URL || "http://localhost:5001"
 
 export const hadlesignup = async (signupdata) => {
   try {
