9c7b89529d5ee6111d8562b6b00aec5036a27428
sujal <sujalkarki1904@gmail.com>
2026-01-02 22:35:15 +0545

    minor change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 <nav className="bg-green-500 text-blue-50 px-4 sm:px-6 md:px-10 lg:px-16 py-3 flex items-center justify-between sticky top-0 z-50 border-b border-white/20">
       {/* Home */}
       <div className="text-xl hover:text-black">
-        <Link to="/">Home</Link>
+        <Link to="/home">Home</Link>
       </div>
 
       {/* Hamburger button: only on small screens */}
